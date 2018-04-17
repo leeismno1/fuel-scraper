bd789d64466bde8a1e2b7cb87863af475130eb38
Lee White <leewhite@Lees-MBP.fritz.box>
2018-04-17 19:01:22 +0800

    fuel_scraper.py

--- fuel_scraper.py
+++ fuel_scraper.py
@@ -3,6 +3,7 @@ from lxml import etree as ET
 import collections, itertools
 from pprint import pprint
 
+# Returns a list of URLs based on the variables for fueld_types, regions and day.
 def gen_fuel(fuel_types, regions, day):
 
     fuel_watch_urls_list = [
@@ -14,7 +15,7 @@ def gen_fuel(fuel_types, regions, day):
 # for each in listed_fuel_watch_urls:
 #    print(each)
 
-
+# Uses the URLs created with function def gen_fuel and from the XML grabs the data and adds it to a dictionary, leaving a list of dictionaries with all data.
 def get_fuel_data(listed_fuel_watch_urls):
     for item in listed_fuel_watch_urls:
         item = requests.get(item)
@@ -42,23 +43,33 @@ def get_fuel_data(listed_fuel_watch_urls):
         return list_of_dicts
 
 
-
+# These variables provide the information to what fuel types, area and day that the gen_fuel function uses to create the required URLS.
 fuel_types = [1, 2, 6]
 regions = [25, 27]
 day = ['today', 'tomorrow']
 
+# The out come of function gen_fuel and the variables for fuel type, region and days.
 listed_fuel_watch_urls = gen_fuel(fuel_types, regions, day)
 
+# This variable is the data provided from the list of dictonaries in funtion get_fuel_data and passes the list of URLs.
 fuel_data = get_fuel_data(listed_fuel_watch_urls)
 
 # pprint(fuel_data, indent=4)
 
 # print(fuel_data)
 
+# converts the list of dictionaries a string.
 fuel_data_string = str(fuel_data)
+
+# Formats the html.
 fuel_data_html = "<html><title>Fuel Report</title><body>" + fuel_data_string + "</body></html>"
 
+# Opens and creates a file named fuel_report.html with write access.
 fuel_file = open('fuel_report.html', 'w')
+
+# Writes the the data from fuel_data_html into the fuel_report.html file.
 fuel_file.write(fuel_data_html)
+
+# Closes fuel_report.html.
 fuel_file.close
 
